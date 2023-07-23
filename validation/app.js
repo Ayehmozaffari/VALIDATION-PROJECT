ab282632b03560b0158058a5589806a87805d0f7
₳ɣēɦ <ayehmozaffari1402@gmail.com>
2023-07-24 00:51:23 +0330

    asdqSD

--- validation/app.js
+++ validation/app.js
@@ -11,4 +11,34 @@
 //         document.querySelector('#submit').disabled = false;
 //     }
 // }
-const container=
+const container=document.querySelector('.container'),
+pwShowHide=document.querySelectorAll('.showHidePw'),
+pwFields=document.querySelectorAll('.password'),
+
+signUp=document.querySelector('.signup-link'),
+login=document.querySelector('.login-link'),
+pwField=document.querySelectorAll('.password');
+// /...................................................................../
+pwShowHide.forEach(eyeIcon=>{
+    eyeIcon.addEventListener("click",()=>{
+        pwFields.forEach(pwField =>{
+            if(pwField.type === "password"){
+                pwField.type="text";
+                pwShowHide.forEach(icon =>{
+                    icon.classList.replace("uil-eye-slash","uil-eye");
+                })
+            }else{
+                pwField.type="password"; 
+                pwShowHide.forEach(icon =>{
+                    icon.classList.replace("uil-eye","uil-eye-slash");
+                })
+            }
+        })
+    })
+})
+signUp.addEventListener("click",( ) =>{
+   container.classList.add("active");
+});
+login.addEventListener("click",( ) =>{
+    container.classList.remove("active");
+ });
