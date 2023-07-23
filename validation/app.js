dd0510c43dba9146c60e1b060f6d986b51e849ab
₳ɣēɦ <ayehmozaffari1402@gmail.com>
2023-07-23 22:41:05 +0330

    rtawer

--- validation/app.js
+++ validation/app.js
@@ -1,13 +1,14 @@
-document.querySelector('#subject').addEventListener('blur',validation)
-document.querySelector('#message').addEventListener('blur',validation)
-document.querySelector('#email').addEventListener('blur',validation)
-function validation(e){
-    if (e.target.value == '')
-    {
-        e.target.style='border:1px solid red;border-top:none;border-left:none;border-right:none;';
+// document.querySelector('#subject').addEventListener('blur',validation)
+// document.querySelector('#message').addEventListener('blur',validation)
+// document.querySelector('#email').addEventListener('blur',validation)
+// function validation(e){
+//     if (e.target.value == '')
+//     {
+//         e.target.style='border:1px solid red;border-top:none;border-left:none;border-right:none;';
         
-    }else{
-        e.target.style='border:1px solid  #296;border-top:none;border-left:none;border-right:none;'
-        document.querySelector('#submit').disabled = false;
-    }
-}
+//     }else{
+//         e.target.style='border:1px solid  #296;border-top:none;border-left:none;border-right:none;'
+//         document.querySelector('#submit').disabled = false;
+//     }
+// }
+const container=
